c0f867eb4625266d03f67316ec455c6be8f4058a
Suju Tan <sujuktan@gmail.com>
2023-03-27 21:20:26 -0400

    Use while loop to log each element in an array

--- packing-list.js
+++ packing-list.js
@@ -9,7 +9,16 @@ const packingList = [
   "cooking utensils",
 ];
 
+//for loop
+// console.log("Kitchen stuff to pack:");
+// for (let i = 0; i < packingList.length; i++) {
+//   console.log(packingList[i]);
+// }
+
+//while loop
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
